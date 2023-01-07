9f96ee515fbbb824b08d243d3efed82a60c2231e
SerGO <akm74@gmx.de>
2023-01-07 10:28:28 +0100

    started project2

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  
+<header>
+  <div>MY PROJECTS</div>
+  <div>Made By SerGolite</div>
+</header>
+<section class="first">
+  <div class="outer">
+    <div class="inner">
+      <div class="bg one">
+        <h2 class="section-heading">Scroll down</h2>
+      </div>
+    </div>
+  </div>
+
+</section>
+<section class="second">
+  <div class="outer">
+    <div class="inner">
+      <div class="bg">
+        <h2 class="section-heading">Animated with GSAP</h2>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="third">
+  <div class="outer">
+    <div class="inner">
+      <div class="bg">
+        <h2 class="section-heading">GreenSock</h2>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="fourth">
+  <div class="outer">
+    <div class="inner">
+      <div class="bg">
+        <h2 class="section-heading">Animation platform</h2>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="fifth">
+  <div class="outer">
+    <div class="inner">
+      <div class="bg">
+        <h2 class="section-heading">Keep scrolling</h2>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+    
+  );
+}
+
+export default App;
